87eea9cbf71f4a143c73e3dd2ebf85608b86afe9
Bala Raman <srbala@gmail.com>
2023-01-02 18:47:49 -0500

    Needles update
    Signed-off-by: Bala Raman <srbala@gmail.com>

--- tests/server_cockpit_default.pm
+++ tests/server_cockpit_default.pm
@@ -7,10 +7,10 @@ use cockpit;
 sub run {
     my $self = shift;
     # check cockpit appears to be enabled and running and firewall is setup
-    assert_script_run 'systemctl enable --now cockpit.socket';
+    assert_script_run 'systemctl enable --now cockpit';
     # TODO: cockpit not enabled by default 
-    assert_script_run 'systemctl is-enabled cockpit.socket';
-    assert_script_run 'systemctl is-active cockpit.socket';
+    assert_script_run 'systemctl is-enabled cockpit';
+    assert_script_run 'systemctl is-active cockpit';
     assert_script_run 'firewall-cmd --query-service cockpit';
     # test cockpit web UI start
     start_cockpit(login => 0);
