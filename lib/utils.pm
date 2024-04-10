a548d404fad1bb1c0e532886501d3d7e23f904f3
Elkhan Mammadli <elkhan.mammadli@protonmail.com>
2024-04-10 14:27:07 +0400

    Add codenames for AlmaLinux OS 9.4 and 8.10
    Signed-off-by: Elkhan Mammadli <elkhan.mammadli@protonmail.com>

--- lib/utils.pm
+++ lib/utils.pm
@@ -71,7 +71,9 @@ sub get_version_major {
 sub get_code_name {
     my $version = get_var("VERSION");
 
-    if ($version eq '9.3') { return "Shamrock Pampas Cat"; }
+    if ($version eq '9.4') { return "Seafoam Ocelot"; }
+    elsif ($version eq '8.10') { return "Cerulean Leopard"; }
+    elsif ($version eq '9.3') { return "Shamrock Pampas Cat"; }
     elsif ($version eq '8.9') { return "Midnight Oncillla"; }
     elsif ($version eq '9.2') { return "Turquoise Kodkod"; }
     elsif ($version eq '8.8') { return "Sapphire Caracal"; }
