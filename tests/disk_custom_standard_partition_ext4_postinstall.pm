5758d0858774f668255386a8fcaf637dd745f651
Andrew Lukoshko <andrew.lukoshko@gmail.com>
2026-09-17 08:23:21 +0000

    Per-arch standard-partition layout checks, plus assorted needles
    disk_custom_standard_partition_ext4_postinstall hardcoded the AlmaLinux
    10 layout, which does not hold for 9: BIOS x86_64 is MBR with no BIOS
    Boot partition, and the /boot and / device numbers differ on aarch64,
    ppc64le, s390x and UEFI x86_64 as well. Split the expectations by arch
    and major version rather than by arch alone.
    
    Also tightens the select_root BIOS needle onto the device name instead
    of a block of surrounding table, and adds getting_started 10.2,
    part_fs 9.8, device_root_resized_nine 9.8, the GDM password-focus and
    test-user-highlighted needles, and a logo-less apps_menu_button.
    
    Committed from the live NFS share, where this had been running
    uncommitted since 2026-04-28.

--- tests/disk_custom_standard_partition_ext4_postinstall.pm
+++ tests/disk_custom_standard_partition_ext4_postinstall.pm
@@ -12,18 +12,23 @@ sub run {
     script_run 'fdisk -l | grep /dev/vda';    # debug
     # AlmaLinux 10.x adds a BIOS Boot partition for BIOS+GPT installs,
     # so partcount is one more than Fedora (which uses MBR for BIOS x86_64).
+    my @maj_ver = split(/\./, get_var("VERSION", ""));
+    my $maj = ($maj_ver[0] // 0);
     my $partcount = 5;
     if (get_var("ARCH") eq "ppc64le") {
         # AL9 ppc64le uses MBR with an extended partition holding /boot,
         # which gives 5 logical partitions = 6 fdisk lines.
         # AL10+ ppc64le uses GPT like x86_64, with 4 primary partitions
         # (PReP boot + / + /boot + swap) = 5 fdisk lines, the default.
-        my @maj_ver = split(/\./, get_var("VERSION", ""));
-        $partcount = 6 if (($maj_ver[0] // 0) < 10);
+        $partcount = 6 if ($maj < 10);
     }
     elsif (get_var("ARCH") eq "s390x") {
         # s390x uses Discoverable Partitions Spec layout: vda1=/, vda2=/boot,
         # vda3=swap. No BIOS Boot or PReP partition.
+        $partcount = 4;
+    }
+    elsif (get_var("ARCH") eq "x86_64" && !get_var("UEFI") && $maj < 10) {
+        # AL9 x86_64 BIOS+MBR: vda1=/, vda2=swap, vda3=/boot. No BIOS Boot.
         $partcount = 4;
     }
     validate_script_output 'fdisk -l | grep /dev/vda | wc -l', sub { $_ =~ m/$partcount/ };
@@ -32,9 +37,36 @@ sub run {
     # vda2=/, vda3=/boot, vda4=swap (matches Fedora vda2/vda3 mapping
     # offset by the BIOS Boot partition). On s390x: vda1=/, vda2=/boot.
     script_run 'mount | grep /dev/vda';    # debug
-    if (get_var("ARCH") eq "s390x") {
+    if (get_var("ARCH") eq "s390x" && $maj < 10) {
+        # AL9 s390x layout: vda1=/, vda2=swap, vda3=/boot
+        validate_script_output "mount | grep /dev/vda3", sub { $_ =~ m/on \/boot type ext4/ };
+        validate_script_output "mount | grep /dev/vda1", sub { $_ =~ m/on \/ type ext4/ };
+    }
+    elsif (get_var("ARCH") eq "s390x") {
+        # AL10+ s390x Discoverable Partitions Spec: vda1=/, vda2=/boot, vda3=swap
         validate_script_output "mount | grep /dev/vda2", sub { $_ =~ m/on \/boot type ext4/ };
         validate_script_output "mount | grep /dev/vda1", sub { $_ =~ m/on \/ type ext4/ };
+    }
+    elsif (get_var("ARCH") eq "aarch64" && $maj < 10) {
+        # AL9 aarch64 UEFI layout: vda1=EFI, vda2=/, vda3=swap, vda4=/boot
+        validate_script_output "mount | grep /dev/vda4", sub { $_ =~ m/on \/boot type ext4/ };
+        validate_script_output "mount | grep /dev/vda2", sub { $_ =~ m/on \/ type ext4/ };
+    }
+    elsif (get_var("ARCH") eq "x86_64" && !get_var("UEFI") && $maj < 10) {
+        # AL9 x86_64 BIOS+MBR: vda1=/, vda2=swap, vda3=/boot
+        validate_script_output "mount | grep /dev/vda3", sub { $_ =~ m/on \/boot type ext4/ };
+        validate_script_output "mount | grep /dev/vda1", sub { $_ =~ m/on \/ type ext4/ };
+    }
+    elsif (get_var("ARCH") eq "x86_64" && get_var("UEFI") && $maj < 10) {
+        # AL9 x86_64 UEFI+GPT: vda1=EFI, vda2=/, vda3=swap, vda4=/boot
+        validate_script_output "mount | grep /dev/vda4", sub { $_ =~ m/on \/boot type ext4/ };
+        validate_script_output "mount | grep /dev/vda2", sub { $_ =~ m/on \/ type ext4/ };
+    }
+    elsif (get_var("ARCH") eq "ppc64le" && $maj < 10) {
+        # AL9 ppc64le MBR with extended partition holding /boot:
+        # vda1=PReP Boot, vda2=/, vda3=swap, vda4=Ext'd, vda5=/boot
+        validate_script_output "mount | grep /dev/vda5", sub { $_ =~ m/on \/boot type ext4/ };
+        validate_script_output "mount | grep /dev/vda2", sub { $_ =~ m/on \/ type ext4/ };
     }
     else {
         validate_script_output "mount | grep /dev/vda3", sub { $_ =~ m/on \/boot type ext4/ };
